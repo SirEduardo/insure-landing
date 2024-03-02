ce4f0513e716ce619c06ebe6c2e022f38cf9682a
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-03-02 11:11:03 +0100

    interactive-cards

--- interactive-cards/src/App.jsx
+++ interactive-cards/src/App.jsx
@@ -1,8 +1,16 @@
-import { Form } from './form'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+      
+  const [Confirm, setConfirm] = useState(false)
+
+  const handleButton = () =>{
+    setConfirm(!Confirm)
+  }
+  
+  
 
   return (
     <main className='container'>
@@ -26,8 +34,39 @@ function App() {
     </div>
   </div>
   </aside>
+ 
+    <div className='form-container'>
+      {!Confirm ? (
+      <div className='form'>
+        <p>CARDHOLDER NAME</p>
+        <input type='text' className='name' placeholder='e.g.Jane Appleseed'/>
+        <p>CARD NUMBER</p>
+        <input type='text' className='number' placeholder='e.g. 1234 5678 9123 0000'/>
+        <div className='div-form'>
+          <div className='exp-date'>
+          <p>EXP.DATE (MM/YY)</p>
+          <input type='text' className='month' placeholder='MM'/>
+          <input type='text' className='year' placeholder='YY'/>
+          </div>
+          <div className='cvc-container'>
+          <p>CVC</p>
+          <input type='text' className='cvc' placeholder='e.g.123'/>
+          </div>
+        </div>
+      <button onClick={handleButton}>Confirm</button>
+      </div>
+      ):(
+        <div className="thank-you">
+        <img src="../images/icon-complete.svg" alt="Complete icon" key="complete-icon"/>
+        <strong>THANK YOU!</strong>
+        <p>We`ve added your card details</p>
+        <button onClick={handleButton}>Continue</button>
+        </div>
+      )}
+    </div>
+  
 
-    <Form/>
+    
     </main>
   )
 }
